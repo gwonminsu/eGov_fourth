4f884ac35af1b8ff72b365dacfc71672b4196730
gwonminsu <kd0615@naver.com>
2025-05-26 13:02:38 +0900

    예약 등록 시 검증 추가(제한인원 수 초과)
    예약 등록 시 제한 인원 수를 초과하는지 검증 기능 추가

--- src/main/java/egovframework/fourth/homework/web/BookingController.java
+++ src/main/java/egovframework/fourth/homework/web/BookingController.java
@@ -16,6 +16,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import egovframework.fourth.homework.service.BookingService;
 import egovframework.fourth.homework.service.BookingVO;
+import egovframework.fourth.homework.service.ProgramScheduleService;
+import egovframework.fourth.homework.service.ProgramScheduleVO;
 
 @RestController
 @RequestMapping("/api/booking")
@@ -27,19 +29,29 @@ public class BookingController {
 
 	@Resource(name = "bookingService")
 	private BookingService bookingService;
+	
+	@Resource(name = "programScheduleService")
+	private ProgramScheduleService programScheduleService;
 
 	// 프로그램 일정에 예약 등록
 	@PostMapping(value = "/createBooking.do", consumes = "application/json", produces = "application/json")
 	public Map<String, String> writeBooking(@RequestBody BookingVO vo) throws Exception {
 		// 예약에 예약인 정보가 없으면 튕굼
-		if (vo.getBookerList() == null) {
-        	log.info("예약인 정보 없음: 예약 등록 거부");
-        	return Collections.singletonMap("error","REJECTED");
-		} else {
-			bookingService.createBooking(vo); // 예약과 예약인 리스트 등록
-			log.info("예약인 정보 존재 확인: 예약 등록 승인");
-			return Collections.singletonMap("status", "OK");
+		if (vo.getBookerList().isEmpty()) {
+        	log.info("예약 등록 거부: 예약인 정보 없음");
+        	return Collections.singletonMap("error","예약인원 정보를 최소 하나 이상 등록하셔야 합니다.");
 		}
+		
+		ProgramScheduleVO schedule = programScheduleService.getProgramSchedule(vo.getProgramScheduleIdx());
+		// 일정의 capacity보다 기존 예약인 수 + 등록하는 예약인 수가 많으면 튕굼
+		if (schedule.getBookerCount() + vo.getBookerList().size() > schedule.getCapacity()) {
+			log.info("예약 등록 거부: 제한 인원 수 초과");
+			return Collections.singletonMap("error","예약하려는 인원 수가 일정의 제한 인원 수를 초과했습니다.");
+		}
+		
+		bookingService.createBooking(vo); // 예약과 예약인 리스트 등록
+		log.info("예약 등록 승인: 예약인 정보 존재 확인: ");
+		return Collections.singletonMap("status", "OK");
 	}
 	
 //	{
